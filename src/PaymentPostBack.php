ffdf0389b312f65aacb935680319619060e64209
Benoth <benoth83@gmail.com>
2015-08-26 11:18:56 +0200

    Payment Post Back Class

---
+++ src/PaymentPostBack.php
@@ -0,0 +1,121 @@
+<?php namespace Benoth\BoaCompra;
+
+/**
+ * Send the postback to BoaCompra to confirm the order information
+ *
+ * Based on documentation v2.48
+ */
+class PaymentPostBack
+{
+    /* BoaCompra Billing URL - must be sent through a POST method */
+    const POSTBACK_URL = 'https://billing.boacompra.com/boacompra.php';
+    const POSTBACK_TEST_URL = 'https://billing.boacompra.com/boacompra_test.php';
+
+    /* PaymentNotification received from BoaCompra */
+    protected $notification;
+
+    public function __construct(PaymentNotification $notification)
+    {
+        $this->notification = $notification;
+    }
+
+    public function getPaymentNotification()
+    {
+        return $this->notification;
+    }
+
+    public function validatePayment()
+    {
+        $code = $this->getReturnCode();
+        if ($code === 0 or $code === 1) {
+            // 0 - Order successfully confirmed.
+            // 1 - Order already confirmed.
+            return true;
+        } elseif ($code === 2) {
+            throw new \Exception('Incorrect parameters values');
+        } elseif ($code === 3) {
+            throw new \Exception('Order not found');
+        } elseif ($code === 4) {
+            throw new \Exception('Postback is missing data');
+        } elseif ($code === 5) {
+            throw new \Exception('Order not paid yet');
+        } elseif ($code === 6) {
+            throw new \Exception('Error reported by the Virtual Store');
+        } elseif ($code === 7) {
+            throw new \Exception('Value for "hash_key" parameter is incorrect');
+        } elseif ($code === 9) {
+            throw new \Exception('Wrong postback url. Please check the "Test Environment" section');
+        }
+        throw new \Exception('Unknown return code "'.$code.'"');
+    }
+
+    protected function getPostFields()
+    {
+        $fields = array(
+            'store_id'         => $this->notification->getPayment()->getVirtualStoreIdentification()->getStoreId(),
+            'transaction_id'   => $this->notification->getTransactionId(),
+            'order_id'         => $this->notification->getPayment()->getOrderId(),
+            'amount'           => $this->notification->getPayment()->getAmount(),
+            'currency_code'    => $this->notification->getCurrencyCode(),
+            'payment_id'       => $this->notification->getPaymentId(),
+            'country_payment'  => $this->notification->getPayment()->getCountryIso(),
+            'customer_country' => $this->notification->getPayment()->getEndUser()->getCountry(),
+            'cmd'              => '_code-notify',
+            'hash_key'         => $this->generateHashKey(),
+        );
+
+        return $fields;
+    }
+
+    protected function generateHashKey()
+    {
+        return md5(
+            $this->notification->getPayment()->getVirtualStoreIdentification()->getStoreId().
+            $this->notification->getTransactionId().
+            $this->notification->getPayment()->getOrderId().
+            $this->notification->getPayment()->getAmount().
+            $this->notification->getCurrencyCode().
+            $this->notification->getPayment()->getVirtualStoreIdentification()->getKey()
+        );
+    }
+
+    protected function getURL()
+    {
+        if ($this->notification->getPayment()->getTestMode()) {
+            return static::POSTBACK_TEST_URL;
+        }
+
+        return static::POSTBACK_URL;
+    }
+
+    protected function curlRequest()
+    {
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_URL, $this->getURL());
+        curl_setopt($curl, CURLOPT_POST, true);
+        curl_setopt($curl, CURLOPT_POSTFIELDS, $this->getPostFields());
+        curl_setopt($curl, CURLOPT_TIMEOUT, 120);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+
+        $response = curl_exec($curl);
+        $infos    = curl_getinfo($curl);
+        curl_close($curl);
+
+        return array($response, $infos);
+    }
+
+    protected function getReturnCode()
+    {
+        list($response, $infos) = $this->curlRequest();
+
+        if (mb_strlen($response) === 0) {
+            throw new \Exception('Empty response (HTTP response code : '.$infos['http_code'].')');
+        }
+
+        if (strpos($response, 'CODRET=') === false or mb_strlen($response) < 8) {
+            throw new \Exception('No return code provided (HTTP response code : '.$infos['http_code'].')');
+        }
+
+        return (int) str_replace('CODRET=', '', trim($response));
+    }
+}
---
+++ tests/PaymentPostBackTest.php
@@ -0,0 +1,207 @@
+<?php
+
+use Benoth\BoaCompra\VirtualStoreIdentification;
+use Benoth\BoaCompra\EndUser;
+use Benoth\BoaCompra\Payment;
+use Benoth\BoaCompra\PaymentNotification;
+use Benoth\BoaCompra\PaymentPostBack;
+
+class PaymentPostBackTest extends PHPUnit_Framework_TestCase
+{
+    protected $postback;
+    protected $stub;
+
+    public function setUp()
+    {
+        $vsi          = new VirtualStoreIdentification('12', 'qwerty');
+        $endUser      = new EndUser('me@example.com');
+        $payment      = new Payment($vsi, $endUser, 'http://localhost.dev/test.php', 'http://localhost.dev/notify.php', 'EUR', '42', 'This is a test order', 1200);
+        $notification = new PaymentNotification($payment, '12', 'QWERTY-0123456', '42', '1200', 'EUR', '1');
+
+        $this->postback = new PaymentPostBack($notification);
+
+        $this->stub = $this->getMockBuilder('Benoth\BoaCompra\PaymentPostBack')
+                     ->setMethods(array('curlRequest'))
+                     ->setConstructorArgs(array($notification))
+                     ->getMock();
+
+        $this->postfields = array(
+          'store_id'         => '12',
+          'transaction_id'   => 'QWERTY-0123456',
+          'order_id'         => '42',
+          'amount'           => 1200,
+          'currency_code'    => 'EUR',
+          'payment_id'       => '1',
+          'country_payment'  => null,
+          'customer_country' => null,
+          'cmd'              => '_code-notify',
+          'hash_key'         => '1c5311317b3aa6eb739bd2ebefa315d8',
+        );
+    }
+
+    public function testSimple()
+    {
+        $this->assertInstanceOf('Benoth\BoaCompra\PaymentPostBack', $this->postback);
+        $this->assertInstanceOf('Benoth\BoaCompra\PaymentNotification', $this->postback->getPaymentNotification());
+    }
+
+    public function testGetPostFields()
+    {
+        // Use reflection to make the method accessible
+        $reflection = new \ReflectionClass(get_class($this->postback));
+        $method = $reflection->getMethod('getPostFields');
+        $method->setAccessible(true);
+
+        $this->assertEquals($this->postfields, $method->invokeArgs($this->postback, array()));
+    }
+
+    public function testGetUrlProd()
+    {
+        // Use reflection to make the method accessible
+        $reflection = new \ReflectionClass(get_class($this->postback));
+        $method = $reflection->getMethod('getURL');
+        $method->setAccessible(true);
+
+        $this->assertEquals(PaymentPostBack::POSTBACK_URL, $method->invokeArgs($this->postback, array()));
+    }
+
+    public function testGetUrlTest()
+    {
+        $this->postback->getPaymentNotification()->getPayment()->setTestMode(1);
+
+        // Use reflection to make the method accessible
+        $reflection = new \ReflectionClass(get_class($this->postback));
+        $method = $reflection->getMethod('getURL');
+        $method->setAccessible(true);
+
+        $this->assertEquals(PaymentPostBack::POSTBACK_TEST_URL, $method->invokeArgs($this->postback, array()));
+    }
+
+    public function testResponseCodeEmptyException()
+    {
+        $this->setExpectedException('Exception', 'Empty response (HTTP response code : 500)');
+
+        $this->stub->expects($this->any())
+             ->method('curlRequest')
+             ->will($this->returnValue(array('', array('http_code' => 500))));
+
+        $this->stub->validatePayment();
+    }
+
+    public function testNoResponseCodeException()
+    {
+        $this->setExpectedException('Exception', 'No return code provided (HTTP response code : 500)');
+
+        $this->stub->expects($this->any())
+             ->method('curlRequest')
+             ->will($this->returnValue(array('CODRET=', array('http_code' => 500))));
+
+        $this->stub->validatePayment();
+    }
+
+    public function testResponseCode0()
+    {
+        $this->stub->expects($this->any())
+             ->method('curlRequest')
+             ->will($this->returnValue(array('CODRET=0', array('http_code' => 200))));
+
+        $this->assertEquals(true, $this->stub->validatePayment());
+    }
+
+    public function testResponseCode1()
+    {
+        $this->stub->expects($this->any())
+             ->method('curlRequest')
+             ->will($this->returnValue(array('CODRET=1', array('http_code' => 200))));
+
+        $this->assertEquals(true, $this->stub->validatePayment());
+    }
+
+    public function testResponseCode2Exception()
+    {
+        $this->setExpectedException('Exception', 'Incorrect parameters values');
+
+        $this->stub->expects($this->any())
+             ->method('curlRequest')
+             ->will($this->returnValue(array('CODRET=2', array('http_code' => 200))));
+
+        $this->assertEquals(true, $this->stub->validatePayment());
+    }
+
+    public function testResponseCode3Exception()
+    {
+        $this->setExpectedException('Exception', 'Order not found');
+
+        $this->stub->expects($this->any())
+             ->method('curlRequest')
+             ->will($this->returnValue(array('CODRET=3', array('http_code' => 200))));
+
+        $this->assertEquals(true, $this->stub->validatePayment());
+    }
+
+    public function testResponseCode4Exception()
+    {
+        $this->setExpectedException('Exception', 'Postback is missing data');
+
+        $this->stub->expects($this->any())
+             ->method('curlRequest')
+             ->will($this->returnValue(array('CODRET=4', array('http_code' => 200))));
+
+        $this->assertEquals(true, $this->stub->validatePayment());
+    }
+
+    public function testResponseCode5Exception()
+    {
+        $this->setExpectedException('Exception', 'Order not paid yet');
+
+        $this->stub->expects($this->any())
+             ->method('curlRequest')
+             ->will($this->returnValue(array('CODRET=5', array('http_code' => 200))));
+
+        $this->assertEquals(true, $this->stub->validatePayment());
+    }
+
+    public function testResponseCode6Exception()
+    {
+        $this->setExpectedException('Exception', 'Error reported by the Virtual Store');
+
+        $this->stub->expects($this->any())
+             ->method('curlRequest')
+             ->will($this->returnValue(array('CODRET=6', array('http_code' => 200))));
+
+        $this->assertEquals(true, $this->stub->validatePayment());
+    }
+
+    public function testResponseCode7Exception()
+    {
+        $this->setExpectedException('Exception', 'Value for "hash_key" parameter is incorrect');
+
+        $this->stub->expects($this->any())
+             ->method('curlRequest')
+             ->will($this->returnValue(array('CODRET=7', array('http_code' => 200))));
+
+        $this->assertEquals(true, $this->stub->validatePayment());
+    }
+
+    public function testResponseCode9Exception()
+    {
+        $this->setExpectedException('Exception', 'Wrong postback url. Please check the "Test Environment" section');
+
+        $this->stub->expects($this->any())
+             ->method('curlRequest')
+             ->will($this->returnValue(array('CODRET=9', array('http_code' => 200))));
+
+        $this->assertEquals(true, $this->stub->validatePayment());
+    }
+
+    public function testResponseCodeUnkownException()
+    {
+        $this->setExpectedException('Exception', 'Unknown return code "999"');
+
+        $this->stub->expects($this->any())
+             ->method('curlRequest')
+             ->will($this->returnValue(array('CODRET=999', array('http_code' => 200))));
+
+        $this->assertEquals(true, $this->stub->validatePayment());
+    }
+}
