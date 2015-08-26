721b27168685c3ebd3b5fe1ef5852e96a469d47f
Benoth <benoth83@gmail.com>
2015-08-26 11:16:37 +0200

    Virtual Store Identification Class

---
+++ src/VirtualStoreIdentification.php
@@ -0,0 +1,35 @@
+<?php namespace Benoth\BoaCompra;
+
+/**
+ * BoaCompra Virtual Store Identification
+ *
+ * Based on documentation v2.48
+ */
+class VirtualStoreIdentification
+{
+    /* Virtual Store Identification on BoaCompra (max length 6) */
+    protected $storeId;
+
+    /* Virtual Store Identification on BoaCompra */
+    protected $key;
+
+    public function __construct($storeId, $key)
+    {
+        if (!ctype_digit($storeId) or mb_strlen((string) $storeId) > 6) {
+            throw new \Exception('Store ID must be an integer with max length of 6');
+        }
+
+        $this->storeId = $storeId;
+        $this->key = $key;
+    }
+
+    public function getStoreId()
+    {
+        return $this->storeId;
+    }
+
+    public function getKey()
+    {
+        return $this->key;
+    }
+}
---
+++ tests/VirtualStoreIdentificationTest.php
@@ -0,0 +1,30 @@
+<?php
+
+use Benoth\BoaCompra\VirtualStoreIdentification;
+
+class VirtualStoreIdentificationTest extends PHPUnit_Framework_TestCase
+{
+    public function testGetters()
+    {
+        $store_id = '1';
+        $key      = 'azerty';
+        $vsi = new VirtualStoreIdentification($store_id, $key);
+
+        $this->assertEquals($store_id, $vsi->getStoreId());
+        $this->assertEquals($key, $vsi->getKey());
+    }
+
+    public function testStoreIdExceptionNumeric()
+    {
+        $this->setExpectedException('Exception', 'Store ID must be an integer with max length of 6');
+
+        $vsi = new VirtualStoreIdentification('azerty', 'azerty');
+    }
+
+    public function testStoreIdExceptionLength()
+    {
+        $this->setExpectedException('Exception', 'Store ID must be an integer with max length of 6');
+
+        $vsi = new VirtualStoreIdentification('azertyuiop', 'azerty');
+    }
+}
