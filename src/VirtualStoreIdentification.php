3bfa30c5c28a0b90d63634ae3ac327da068ec556
Benoth <benoth83@gmail.com>
2015-08-27 13:41:56 +0200

    VirtualStoreIdentification DocBlock

--- src/VirtualStoreIdentification.php
+++ src/VirtualStoreIdentification.php
@@ -13,6 +13,14 @@ class VirtualStoreIdentification
     /* Virtual Store Identification on BoaCompra */
     protected $key;
 
+    /**
+     * Create a new Virtual Store Identification
+     *
+     * @param string $storeId Virtual Store ID
+     * @param string $key     Virtual Store Secret Key
+     *
+     * @throws Exception If the provided Store ID doesn't contain only numbers or more than 6 characters
+     */
     public function __construct($storeId, $key)
     {
         if (!ctype_digit($storeId) || mb_strlen((string) $storeId) > 6) {
@@ -23,11 +31,21 @@ class VirtualStoreIdentification
         $this->key     = $key;
     }
 
+    /**
+     * Get the Store ID
+     *
+     * @return string
+     */
     public function getStoreId()
     {
         return $this->storeId;
     }
 
+    /**
+     * Get the Store Secret Key
+     *
+     * @return string
+     */
     public function getKey()
     {
         return $this->key;
