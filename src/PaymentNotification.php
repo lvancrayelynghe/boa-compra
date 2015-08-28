2a5051856a35c4203638b53249c6318ff8219b45
Benoth <benoth83@gmail.com>
2015-08-28 13:26:30 +0200

    PaymentNotification DocBlock

--- src/PaymentNotification.php
+++ src/PaymentNotification.php
@@ -29,6 +29,20 @@ class PaymentNotification
         '200.147.106.66',
     );
 
+    /**
+     * Create a new BoaCompra notification checker
+     * All parameters (except $payment) must the datas received by distant server
+     * (generally from $_POST except for $ipAddress which generallyis taken in $_SERVER).
+     *
+     * @param Payment $payment
+     * @param string  $storeId
+     * @param string  $transactionId
+     * @param string  $orderId
+     * @param string  $amount
+     * @param string  $currencyCode
+     * @param string  $paymentId
+     * @param string  $ipAddress
+     */
     public function __construct(Payment $payment, $storeId, $transactionId, $orderId, $amount, $currencyCode, $paymentId, $ipAddress = null)
     {
         $this->payment = $payment;
@@ -44,26 +58,61 @@ class PaymentNotification
         $this->paymentId     = $paymentId;
     }
 
+    /**
+     * Get the payment object.
+     *
+     * @return Payment
+     */
     public function getPayment()
     {
         return $this->payment;
     }
 
+    /**
+     * Get the Transaction Id sent by BoaCompra.
+     *
+     * @return string
+     */
     public function getTransactionId()
     {
         return $this->transactionId;
     }
 
+    /**
+     * Get the Currency Code sent by BoaCompra.
+     *
+     * @return string
+     */
     public function getCurrencyCode()
     {
         return $this->currencyCode;
     }
 
+    /**
+     * Get the Payment Id (End User payment method) sent by BoaCompra.
+     *
+     * @return string
+     */
     public function getPaymentId()
     {
         return $this->paymentId;
     }
 
+    /**
+     * Validate the datas sent by BoaCompra.
+     *
+     * @param string $storeId
+     * @param string $orderId
+     * @param string $amount
+     * @param string $currencyCode
+     * @param string $paymentId
+     *
+     * @throws Exception If the provided $storeId is different from the one defined in VirtualStoreIdentification
+     * @throws Exception If the provided $orderId is different from the one defined in Payment
+     * @throws Exception If the provided $amount is different from the one defined in Payment
+     * @throws Exception If the provided $currencyCode is different from the one defined in Payment (if initially chosen)
+     * @throws Exception If the provided $paymentId is different from the one defined in Payment (if initially chosen)
+     */
     protected function validateReturnedDatas($storeId, $orderId, $amount, $currencyCode, $paymentId)
     {
         if ($storeId != $this->payment->getVirtualStoreIdentification()->getStoreId()) {
@@ -89,10 +138,21 @@ class PaymentNotification
         }
     }
 
+    /**
+     * Check if the IP Address is one from BoaCompra servers.
+     *
+     * @param string $ipAddress
+     *
+     * @throws Exception If the provided $ipAddress is unauthorized
+     *
+     * @return bool
+     */
     protected function validateIpAddress($ipAddress)
     {
         if (!in_array($ipAddress, $this->authorizedIps)) {
             throw new \Exception('Unauthorized IP Adress ('.$ipAddress.')');
         }
+
+        return true;
     }
 }
